a12568828db7cf393fb3e1fe8aa0c9b772bd08ee
gnerkus <ifeanyioraelosi@gmail.com>
2017-08-22 13:33:42 +0200

    feat(todos, projects): add buttons

--- projects/index.js
+++ projects/index.js
@@ -1,21 +1,33 @@
 const express = require('express')
-const http = require('http')
-const redis = require('redis')
+const bodyParser = require('body-parser')
+const path = require('path')
+// const redis = require('redis')
 
 const app = express()
+app.use(bodyParser.json())
 // Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-const redisClient = redis.createClient('6379', 'redis-data')
+// const redisClient = redis.createClient('6379', 'redis-data')
 const PORT = process.env.PORT || 1337
 
 app.get('/', (req, res, next) =>
-  redisClient.incr('projects', (err, counter) => {
-    if (err) return next(err)
-    res.send(`PROJECTS has been viewed ${counter} times!`)
-  })
+  // redisClient.incr('projects', (err, counter) => {
+  //   if (err) return next(err)
+  //   res.send(`PROJECTS has been viewed ${counter} times!`)
+  // })
+  res.sendFile(path.join(`${__dirname}/projects.html`))
+  // The server receives messages from RabbitMQ and sends a
+  // HTML page depending on the message received
 )
 
-http
-  .createServer(app)
+app.post('/todos', (req, res, next) => {
+  res.json(req.body)
+})
+
+app.get('/projects', (req, res, next) => {
+  res.json({color: 'purple-box'})
+})
+
+app
   .listen(PORT, () =>
     console.log(`Listening on port ${PORT}`)
   )
