3ffd54efbe305ab38926379a3e3c933279dd171b
gnerkus <ifeanyioraelosi@gmail.com>
2017-10-10 15:50:22 +0200

    [catalog] create Redis message queues
    - Define a route to update the sku on the Redis message queue.

--- catalog/src/server.js
+++ catalog/src/server.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import express from 'express'
 import morgan from 'morgan'
+import RedisSMQ from 'rsmq'
 import path from 'path'
 import renderPage from './page/render'
 
 const app = express()
+const rsmqClient = new RedisSMQ({host: 'redis-data', port: 6379, ns: 'cf'})
 app.use(morgan('dev'))
 
 app.set('views', path.join(__dirname, 'views'))
@@ -13,11 +15,35 @@ app.set('view engine', 'ejs')
 app.use('/catalog/images', express.static('./images'))
 app.use('/catalog', express.static('./build'))
 
+// The catalog service is responsible for creating Redis queues.
+rsmqClient.createQueue({qname: 'sku'}, (err, res) => {
+  if (res === 1) {
+    console.log('queue created')
+  }
+})
+
+rsmqClient.createQueue({qname: 'basket'}, (err, res) => {
+  if (res === 1) {
+    console.log('queue created')
+  }
+})
+
 app.get('/:sku?', (req, res) => {
   const sku = req.params.sku
   const html = renderPage(sku)
 
   res.render('layout', { html })
+})
+
+// Notify all other services that a tractor has been selected
+app.post('/sku', (req, res, next) => {
+  rsmqClient.sendMessage({qname: 'sku', message: req.body.sku}, (err, resp) => {
+    if (resp) {
+      res.json({msgstatus: 'sent', msgid: resp})
+    } else {
+      res.json({msgstatus: 'not sent'})
+    }
+  })
 })
 
 app.listen(3003);
