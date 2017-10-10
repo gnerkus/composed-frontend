91d87e5177df6dc76c5f6d14b658defea337e713
gnerkus <ifeanyioraelosi@gmail.com>
2017-10-10 13:28:11 +0200

    [catalog] render options using HTML
    The catalog service serves as the component host (for now). As such, it is meant to be framework-agnostic.
    
    The ProductOption component has been removed in favour of HTML as React is phased from this service.

---
+++ catalog/src/page/render.js
@@ -0,0 +1,54 @@
+/* globals window */
+// The catalog service renders the content from the other services
+// (basket, recommendations).
+const product = {
+  name: 'Tractor',
+  variants: [
+    {
+      sku: 't_porsche',
+      color: 'red',
+      name: 'Porsche-Diesel Master 419',
+      image: '/catalog/images/tractor-red.jpg',
+      thumb: '/catalog/images/tractor-red-thumb.jpg',
+      price: '66,00 €',
+    },
+    {
+      sku: 't_fendt',
+      color: 'green',
+      name: 'Fendt F20 Dieselroß',
+      image: '/catalog/images/tractor-green.jpg',
+      thumb: '/catalog/images/tractor-green-thumb.jpg',
+      price: '54,00 €',
+    },
+    {
+      sku: 't_eicher',
+      color: 'blue',
+      name: 'Eicher Diesel 215/16',
+      image: '/catalog/images/tractor-blue.jpg',
+      thumb: '/catalog/images/tractor-blue-thumb.jpg',
+      price: '58,00 €',
+    }
+  ]
+}
+
+function renderOption(variant, sku) {
+  const active = sku === variant.sku ? 'active' : ''
+
+  return `
+    <a href="/${variant.sku}" class="${active}" type="button" data-sku="${variant.sku}">
+      <img src="${variant.thumb}" alt="${variant.name}" />
+    </a>
+  `
+}
+
+export default function renderPage(sku = 't_porsche') {
+  const variant = product.variants.find(v => sku === v.sku)
+
+  if (!variant) { return '<pre>no product not found</pre>' }
+
+  return `
+    <div id="image"><div><img src="${variant.image}" alt="${variant.name}" /></div></div>
+    <h2 id="name">${product.name} <small>${variant.name}</small></h2>
+    <div id="options">${product.variants.map(v => renderOption(v, sku)).join('')}</div>
+  `
+}
