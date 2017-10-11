b491b6fb9980ee73c9ee7553b33d3a7e5b17aa55
gnerkus <ifeanyioraelosi@gmail.com>
2017-10-11 14:18:44 +0200

    [basket] render basket components as React components.

--- basket/src/client.js
+++ basket/src/client.js
@@ -1,4 +1,26 @@
 /* globals window */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import BasketBuy from './cf-basket-buy/BasketBuy'
 import BasketBasket from './cf-basket-basket/BasketBasket'
+
+ReactDOM.render(
+  React.createElement(
+    BasketBuy,
+    null,
+    null
+  ),
+  document.getElementById('buy')
+)
+
+ReactDOM.render(
+  React.createElement(
+    BasketBasket,
+    null,
+    null
+  ),
+  document.getElementById('basket')
+)
+
 window.basket = { count: 0 }
